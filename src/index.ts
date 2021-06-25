c1db4add4672e04ea96cea58df6a9ed181720c5a
Mac Bubb <bubbem@gmail.com>
2021-06-25 10:33:38 -0700

    removed html portion, need to code interface to use function beyond console

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,5 @@
 document.getElementById("app").innerHTML = `
-<h1>smallest multiple (euler 5)</h1>
-<div>
-  Look
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>
-  for more info about Parcel.
-</div>
+
 `;
 
 const isPrime = (num: number): boolean => {
